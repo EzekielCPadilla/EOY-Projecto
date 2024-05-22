332df3488fecf536bdb8d69032d443f5149d282f
Ezekiel Padilla <141200071+EzekielCPadilla@users.noreply.github.com>
2024-05-22 16:45:27 +0000

    COMMIT N PUSHHHSHH

---
+++ GamePanel.java
@@ -0,0 +1,29 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+public class GamePanel extends JPanel implements ActionListener{
+    GamePanel(){
+        
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple() {
+
+    }
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
